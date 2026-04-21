bf581f4a14ec7bf2ae7e8330cc1c21703ebc28e9
Dollant <ariawilliams2425@gmail.com>
2026-04-21 13:52:00 -0400

    Added getters and setters to help calculate the total gross pay.

--- src/main/java/com/pluarlsight/Employee.java
+++ src/main/java/com/pluarlsight/Employee.java
@@ -13,5 +13,21 @@ public class Employee {
         this.hoursWorked = hoursWorked;
         this.payRate = payRate;
     }
+
+    public int getEmployeeId() { return employeeId; }
+    public void  setEmployeeId(int employeeId) { this.employeeId = employeeId; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public double getHoursWorked() { return hoursWorked; }
+    public void setHoursWorked(double hoursWorked) { this.hoursWorked = hoursWorked; }
+
+    public double getPayRate() { return payRate; }
+    public void setPayRate(double payRate) { this.payRate = payRate; }
+
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
 }
 
