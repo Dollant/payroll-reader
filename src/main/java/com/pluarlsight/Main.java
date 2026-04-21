463dc64e154bb9d6e907266f72904969b7ca3392
Dollant <ariawilliams2425@gmail.com>
2026-04-21 14:46:50 -0400

    Added Try/Catch, path to employee files, and a skip to the header line (id|name|hours-worked|pay-rate).

--- src/main/java/com/pluarlsight/Main.java
+++ src/main/java/com/pluarlsight/Main.java
@@ -8,5 +8,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String fileName = "src/main/resources/employees.csv";
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            String line;
+
+            reader.readLine();
+        } catch (IOException e) {
+
+        }
+
+
+
     }
 }
